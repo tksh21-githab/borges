935f2c99a8936b106b13d123750bb4a1baa92928
tksh21-githab <tksh21+githab@gmail.com>
2026-04-06 06:20:46 +0900

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "tksh21-githab.github.io/borges",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
